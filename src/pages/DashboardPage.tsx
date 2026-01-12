520ca98d38176bd5da085fc11c602b0915658a4b
vincenzo <arc.vincenzo.piscopo@gmail.com>
2026-01-12 18:09:10 +0100

    Aumentato z-index modal per sovrapporsi alla mappa

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -705,7 +705,7 @@ const DashboardPage: React.FC = () => {
 
       {/* Add POI Modal - appears as overlay when clicking on map */}
       {showAddForm && newPoiLocation && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[1000]">
           <div className="bg-white rounded-lg shadow-xl max-w-md w-full mx-4">
             <div className="p-4">
               <h3 className="text-lg font-semibold text-gray-900 mb-4 text-center">
